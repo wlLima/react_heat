e3cc4c57f3119592619757de12f646334c3aafa7
wlLima <lima14.wl@gmail.com>
2021-10-19 22:04:43 -0300

    ✨  basics layout

---
+++ src/components/LoginBox/index.tsx
@@ -0,0 +1,19 @@
+import { VscGithubInverted } from 'react-icons/vsc'
+
+import styles from './styles.module.scss';
+
+
+export function LoginBox() {
+
+    return (
+        <div className={styles.loginBoxWrapper}>
+            <strong>Envie e compartilhe sua mensagem</strong>
+
+            <a href="#" className={styles.signInWithGithub}>
+                <VscGithubInverted size="24" />
+                Entrar com Github
+            </a>
+        </div>
+    );
+
+}
